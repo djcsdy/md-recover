71290339e4452dac054b7e2201c9c9dfd8033d5d
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-09-11 05:34:27 +0100

    Implement skeleton CLI interface.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+use os_display::Quotable;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Options {
+    devices: Vec<PathBuf>,
+
+    #[arg(short, long)]
+    output: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::parse();
+
+    println!("Attempting to recover data from:");
+
+    for device in options.devices {
+        println!(" * {}", device.maybe_quote())
+    }
+
+    println!();
+
+    println!("Recovered files will be written to:");
+
+    for path in options.output {
+        println!(" * {}", path.maybe_quote());
+    }
 }
