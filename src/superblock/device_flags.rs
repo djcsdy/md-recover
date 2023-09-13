0ab31cbc399da5fe562dcb4b7f118f0088d6c27b
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-09-13 23:51:08 +0100

    Implement LayoutAs<u8> for DeviceFlags.

--- src/superblock/device_flags.rs
+++ src/superblock/device_flags.rs
@@ -1,6 +1,18 @@
+use binary_layout::LayoutAs;
+
 bitflags! {
     pub struct DeviceFlags: u8 {
         const WRITE_MOSTLY = 1;
         const FAIL_FAST = 2;
+    }
+}
+
+impl LayoutAs<u8> for DeviceFlags {
+    fn read(v: u8) -> Self {
+        Self::from_bits_retain(v)
+    }
+
+    fn write(v: Self) -> u8 {
+        v.bits()
     }
 }
