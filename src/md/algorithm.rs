99e5c31abfeff3d7c3622c0c0a3b8a7a3f21a3ba
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-11-08 20:41:17 +0000

    Add MdAlgorithm::from_level_and_layout function.

--- src/md/algorithm.rs
+++ src/md/algorithm.rs
@@ -2,10 +2,17 @@ use crate::md::raid5::Raid5Algorithm;
 use crate::md::raid6::Raid6Algorithm;
 
 pub enum MdAlgorithm {
-    Unsupported {
-        level: u32,
-        layout: u32,
-    },
+    Unsupported { level: u32, layout: u32 },
     Raid5(Raid5Algorithm),
     Raid6(Raid6Algorithm),
+}
+
+impl MdAlgorithm {
+    pub fn from_level_and_layout(level: u32, layout: u32) -> Self {
+        match level {
+            5 => Raid5Algorithm::from_layout(layout).map(|algorithm| Self::Raid5(algorithm)),
+            6 => Raid6Algorithm::from_layout(layout).map(|algorithm| Self::Raid6(algorithm)),
+            _ => None
+        }.unwrap_or_else(|| Self::Unsupported { level, layout })
+    }
 }
