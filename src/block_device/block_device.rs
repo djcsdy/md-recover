cdffe1c2fdb6268d74b1615d4ca99e4cef29989f
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-27 20:14:49 +0100

    Delete convenience functions for BlockDevice.
    It makes more sense to use the functions on
    NativeBlockDevice directly.

--- src/block_device/block_device.rs
+++ src/block_device/block_device.rs
@@ -1,16 +1,11 @@
-use crate::block_device::native::NativeBlockDevice;
-use std::fs::File;
 use std::io::{Read, Result, Seek};
-use std::path::Path;
 
 pub trait BlockDevice: Read + Seek {
-    fn open_path<P: AsRef<Path>>(path: P) -> Result<NativeBlockDevice> {
-        NativeBlockDevice::open_path(path)
-    }
-
-    fn from_file(file: File) -> NativeBlockDevice {
-        NativeBlockDevice::from_file(file)
-    }
-
     fn size(&mut self) -> Result<u64>;
+}
+
+impl BlockDevice for Box<dyn BlockDevice> {
+    fn size(&mut self) -> Result<u64> {
+        (**self).size()
+    }
 }
