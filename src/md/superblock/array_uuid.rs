139d40cc1e46a19b8b597c612d93014c789eb23b
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-27 21:54:34 +0100

    Simplify ArrayUuid::Long.

--- src/md/superblock/array_uuid.rs
+++ src/md/superblock/array_uuid.rs
@@ -5,10 +5,8 @@ use itertools::Itertools;
 
 pub enum ArrayUuid {
     Short([u8; 4]),
-    Long(LongArrayUuid),
+    Long([u8; 16]),
 }
-
-pub struct LongArrayUuid([u8; 16]);
 
 impl ArrayUuid {
     pub fn from_u32(value: u32) -> Self {
@@ -20,17 +18,11 @@ impl ArrayUuid {
     pub fn from_u32_4(value: &[u32; 4]) -> Self {
         let mut buffer = [0u8; 16];
         LittleEndian::write_u32_into(value.as_ref(), &mut buffer);
-        Self::Long(LongArrayUuid::new(&buffer))
+        Self::Long(buffer)
     }
 
     pub fn from_u8_16(value: &[u8; 16]) -> Self {
-        Self::Long(LongArrayUuid::new(value))
-    }
-}
-
-impl LongArrayUuid {
-    pub fn new(value: &[u8; 16]) -> Self {
-        Self(*value)
+        Self::Long(*value)
     }
 }
 
@@ -38,13 +30,7 @@ impl Display for ArrayUuid {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             ArrayUuid::Short(uuid) => write!(f, "{:02x}", uuid.iter().format("")),
-            ArrayUuid::Long(uuid) => write!(f, "{}", uuid),
+            ArrayUuid::Long(uuid) => write!(f, "{:02x}", uuid.iter().format("")),
         }
-    }
-}
-
-impl Display for LongArrayUuid {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02x}", self.0.iter().format(""))
     }
 }
