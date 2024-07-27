2f5f627a6b5b2a079f5bcef23c9ae16f524c35ca
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-27 21:38:37 +0100

    Add an id to each MdDevice so it can be identified later.

--- src/md/device/device.rs
+++ src/md/device/device.rs
@@ -1,11 +1,13 @@
 use crate::block_device::{BlockDevice, NativeBlockDevice};
+use crate::md::device::id::MdDeviceId;
 use crate::md::superblock::Superblock;
+use crate::md::superblock::{SuperblockVersion0, SuperblockVersion1};
+use std::ffi::OsStr;
 use std::io::{Error, ErrorKind, Result, SeekFrom};
 use std::path::Path;
 
-use crate::md::superblock::{SuperblockVersion0, SuperblockVersion1};
-
 pub struct MdDevice<S: Superblock, D: BlockDevice> {
+    pub id: MdDeviceId,
     pub superblock: S,
     pub minor_version: u32,
     device: D,
@@ -18,24 +20,30 @@ impl<S: Superblock, D: BlockDevice> MdDevice<S, D> {
 
 impl MdDevice<Box<dyn Superblock>, NativeBlockDevice> {
     pub fn open_path<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let device = NativeBlockDevice::open_path(path)?;
-        Self::from_block_device(device)
+        let device = NativeBlockDevice::open_path(path.as_ref())?;
+        Self::from_block_device(device, Some(path.as_ref().as_os_str()))
     }
 }
 
 impl<D: BlockDevice> MdDevice<Box<dyn Superblock>, D> {
-    pub fn from_block_device(mut device: D) -> Result<Self> {
+    pub fn from_block_device<S: AsRef<OsStr>>(
+        mut device: D,
+        user_reference: Option<S>,
+    ) -> Result<Self> {
         let size = device.size()?;
 
         if size < Self::MIN_DEVICE_SIZE {
             return Err(Error::from(ErrorKind::Other));
         }
 
+        let id = MdDeviceId::new(user_reference);
+
         for (minor_version, offset) in [(2, 8 << 9), (1, 0), (0, (((size >> 9) - 16) & !7) << 9)] {
             device.seek(SeekFrom::Start(offset))?;
             match SuperblockVersion1::read(&mut device) {
                 Ok(superblock) => {
                     return Ok(Self {
+                        id,
                         superblock: Box::new(superblock),
                         minor_version,
                         device,
@@ -51,6 +59,7 @@ impl<D: BlockDevice> MdDevice<Box<dyn Superblock>, D> {
             let minor_version = superblock.minor_version();
 
             Ok(Self {
+                id,
                 superblock: Box::new(superblock),
                 minor_version,
                 device,
