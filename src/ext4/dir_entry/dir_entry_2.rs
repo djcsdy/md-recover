f6030c96a3e4d5383efe4ec1ba524be4ee7294e0
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-27 11:04:57 +0100

    Add ext4 DirEntry2 struct.

--- src/ext4/dir_entry/dir_entry_2.rs
+++ src/ext4/dir_entry/dir_entry_2.rs
@@ -5,8 +5,9 @@ use super::NAME_LENGTH;
 define_layout!(layout, LittleEndian, {
     inode: u32,
     record_length: u16,
-    name_length: u16,
+    name_length: u8,
+    file_type: u8,
     name: [u8; NAME_LENGTH]
 });
 
-pub struct DirEntry1<S: AsRef<[u8]>>(layout::View<S>);
+pub struct DirEntry2<S: AsRef<[u8]>>(layout::View<S>);
--- src/ext4/dir_entry/mod.rs
+++ src/ext4/dir_entry/mod.rs
@@ -1,3 +1,7 @@
 mod dir_entry_1;
+mod dir_entry_2;
+
+pub(self) const NAME_LENGTH: usize = 255;
 
 pub use dir_entry_1::DirEntry1;
+pub use dir_entry_2::DirEntry2;
