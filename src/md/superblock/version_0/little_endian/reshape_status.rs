1e5021db23abb4df9eb1a12004b5418f271b66ca
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-29 23:54:11 +0100

    Implement From<ReshapeStatusVersion0> for ReshapeStatus (LittleEndian).

--- src/md/superblock/version_0/little_endian/reshape_status.rs
+++ src/md/superblock/version_0/little_endian/reshape_status.rs
@@ -1,5 +1,6 @@
 use binary_layout::prelude::*;
 
+use crate::md::superblock::reshape_status::ReshapeStatus;
 pub use layout::NestedView as NestedReshapeStatusVersion0;
 pub use layout::View as ReshapeStatusVersion0;
 
@@ -10,3 +11,16 @@ define_layout!(layout, LittleEndian, {
     new_layout: u32,
     new_chunk_size: u32
 });
+
+impl<S: AsRef<[u8]>> From<ReshapeStatusVersion0<S>> for ReshapeStatus {
+    fn from(value: ReshapeStatusVersion0<S>) -> Self {
+        Self {
+            reshape_position: value.reshape_position().read(),
+            new_level: value.new_level().read(),
+            delta_disks: value.delta_disks().read(),
+            new_layout: value.new_layout().read(),
+            new_chunk_size: value.new_chunk_size().read(),
+            new_offset: 0,
+        }
+    }
+}
