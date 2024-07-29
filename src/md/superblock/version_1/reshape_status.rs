e6a398ca7f5dc52b22fcdedd7ed7bc15dc9f8e68
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-29 23:44:50 +0100

    Implement From<ReshapeStatusVersion1> for ReshapeStatus.

--- src/md/superblock/version_1/reshape_status.rs
+++ src/md/superblock/version_1/reshape_status.rs
@@ -1,5 +1,6 @@
 use binary_layout::prelude::*;
 
+use crate::md::superblock::reshape_status::ReshapeStatus;
 pub use layout::NestedView as NestedReshapeStatusVersion1;
 pub use layout::View as ReshapeStatusVersion1;
 
@@ -11,3 +12,16 @@ define_layout!(layout, LittleEndian, {
     new_chunk_size: u32,
     new_offset: u32
 });
+
+impl<S: AsRef<[u8]>> From<ReshapeStatusVersion1<S>> for ReshapeStatus {
+    fn from(value: ReshapeStatusVersion1<S>) -> Self {
+        Self {
+            new_level: value.new_level().read(),
+            reshape_position: value.reshape_position().read(),
+            delta_disks: value.delta_disks().read(),
+            new_layout: value.new_layout().read(),
+            new_chunk_size: value.new_chunk_size().read(),
+            new_offset: value.new_offset().read(),
+        }
+    }
+}
