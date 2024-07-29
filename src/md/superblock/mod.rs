32a0557d7fc40ae6457ca2e7b4d324c5b4dc2327
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-29 23:25:03 +0100

    Superblock: Rename trait.rs to superblock.rs.

--- src/md/superblock/mod.rs
+++ src/md/superblock/mod.rs
@@ -1,9 +1,9 @@
 pub use self::array_uuid::ArrayUuid;
-pub use self::r#trait::Superblock;
+pub use self::superblock::Superblock;
 pub use self::version_0::SuperblockVersion0;
 pub use self::version_1::SuperblockVersion1;
 
 mod array_uuid;
-mod r#trait;
+mod superblock;
 mod version_0;
 mod version_1;
