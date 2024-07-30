fd71833010896055387340429376f469960c6d2e
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-30 05:43:21 +0100

    Add MdArray::diagnose_array_name_problem function.

--- src/md/md_array.rs
+++ src/md/md_array.rs
@@ -4,6 +4,7 @@ use crate::md::device::MdDeviceId;
 use crate::md::superblock::{ArrayUuid, Superblock};
 use crate::md::MdDevice;
 use std::collections::HashMap;
+use std::ffi::OsString;
 
 pub struct MdArray {
     devices: Vec<MdDevice<Box<dyn Superblock>, Box<dyn BlockDevice>>>,
@@ -20,6 +21,21 @@ impl MdArray {
                 .iter()
                 .map(|device| (device.superblock.array_uuid(), device.id.clone())),
         );
+
+        if map.len() > 1 {
+            Some(map)
+        } else {
+            None
+        }
+    }
+
+    fn diagnose_array_name_problem(&self) -> Option<HashMap<OsString, Vec<MdDeviceId>>> {
+        let map = HashMap::from_multi_iter(self.devices.iter().filter_map(|device| {
+            device
+                .superblock
+                .array_name()
+                .map(|array_name| (array_name.into(), device.id.clone()))
+        }));
 
         if map.len() > 1 {
             Some(map)
