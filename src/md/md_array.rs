837f337f0aea589b61450c94de81e01d61966503
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-07-30 05:20:30 +0100

    Add MdArray::diagnose_array_uuid_problem function.

--- src/md/md_array.rs
+++ src/md/md_array.rs
@@ -1,6 +1,8 @@
 use crate::block_device::BlockDevice;
-use crate::md::superblock::Superblock;
+use crate::md::device::MdDeviceId;
+use crate::md::superblock::{ArrayUuid, Superblock};
 use crate::md::MdDevice;
+use std::collections::HashMap;
 
 pub struct MdArray {
     devices: Vec<MdDevice<Box<dyn Superblock>, Box<dyn BlockDevice>>>,
@@ -9,5 +11,26 @@ pub struct MdArray {
 impl MdArray {
     pub fn new(devices: Vec<MdDevice<Box<dyn Superblock>, Box<dyn BlockDevice>>>) -> Self {
         Self { devices }
+    }
+
+    fn diagnose_array_uuid_problem(&self) -> Option<HashMap<ArrayUuid, Vec<MdDeviceId>>> {
+        let mut map = HashMap::new();
+        for device in &self.devices {
+            let array_uuid = device.superblock.array_uuid();
+            match map.get_mut(&array_uuid) {
+                None => {
+                    map.insert(array_uuid, vec![device.id.clone()]);
+                }
+                Some(devices) => {
+                    devices.push(device.id.clone());
+                }
+            }
+        }
+
+        if map.len() > 1 {
+            Some(map)
+        } else {
+            None
+        }
     }
 }
