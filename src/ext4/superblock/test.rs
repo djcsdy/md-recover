8b314d774cc29d4bc094a19b80f7be24b0bbf1a4
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-18 15:03:46 +0000

    Test ext4::Superblock against ext4 superblock dump.

--- src/ext4/superblock/test.rs
+++ src/ext4/superblock/test.rs
@@ -1,6 +1,7 @@
 use crate::ext4::superblock::Superblock;
 
 const EXT2: &[u8] = include_bytes!("test-data/ext2");
+const EXT4: &[u8] = include_bytes!("test-data/ext4");
 
 #[test]
 fn size_of_superblock() {
@@ -8,8 +9,18 @@ fn size_of_superblock() {
 }
 
 #[test]
+fn inodes_count() {
+    assert_eq!(Superblock::new(EXT4).inodes_count(), 64);
+}
+
+#[test]
 fn inodes_count_ext2() {
     assert_eq!(Superblock::new(EXT2).inodes_count(), 64);
+}
+
+#[test]
+fn blocks_count() {
+    assert_eq!(Superblock::new(EXT4).blocks_count(), 128);
 }
 
 #[test]
