d771907eb22b238310b3bff27699676a5d91230b
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-18 12:52:23 +0000

    Add FileBlockDevice.

---
+++ src/block_device/file.rs
@@ -0,0 +1,38 @@
+use crate::block_device::BlockDevice;
+use std::fs::File;
+use std::io::{Read, Result, Seek, SeekFrom};
+use std::os::unix::fs::MetadataExt;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct FileBlockDevice {
+    file: File,
+}
+
+impl FileBlockDevice {
+    pub fn open_path<P: AsRef<Path>>(path: P) -> Result<Self> {
+        File::open(path).map(|file| Self::from_file(file))
+    }
+
+    pub fn from_file(file: File) -> Self {
+        Self { file }
+    }
+}
+
+impl BlockDevice for FileBlockDevice {
+    fn size(&mut self) -> Result<u64> {
+        self.file.metadata().map(|metadata| metadata.size())
+    }
+}
+
+impl Read for FileBlockDevice {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.file.read(buf)
+    }
+}
+
+impl Seek for FileBlockDevice {
+    fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
+        self.file.seek(pos)
+    }
+}
--- src/block_device/mod.rs
+++ src/block_device/mod.rs
@@ -1,5 +1,7 @@
 mod block_device;
+mod file;
 mod native;
 
 pub use block_device::BlockDevice;
+pub use file::FileBlockDevice;
 pub use native::NativeBlockDevice;
