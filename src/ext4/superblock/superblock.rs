e284e1f86526a4fd63bb1b8c03db96540d7482bb
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-19 02:08:20 +0000

    Add ext4::Superblock::max_mount_count function.

--- src/ext4/superblock/superblock.rs
+++ src/ext4/superblock/superblock.rs
@@ -207,6 +207,10 @@ impl<S: AsRef<[u8]>> Superblock<S> {
         self.view().into_mount_count().read()
     }
 
+    pub fn max_mount_count(&self) -> u16 {
+        self.view().into_max_mount_count().read()
+    }
+
     pub fn read_only_compatible_features(&self) -> ReadOnlyCompatibleFeatures {
         self.view().into_read_only_compatible_features().read()
     }
