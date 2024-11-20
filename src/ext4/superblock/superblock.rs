07966e51bb348d1d9c4f2103f8a9a113b434ee6c
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-20 23:46:43 +0000

    Add ext4::Superblock::lost_and_found_inode_number function.

--- src/ext4/superblock/superblock.rs
+++ src/ext4/superblock/superblock.rs
@@ -616,6 +616,10 @@ impl<S: AsRef<[u8]>> Superblock<S> {
             .unwrap()
     }
 
+    pub fn lost_and_found_inode_number(&self) -> u32 {
+        self.view().into_lost_and_found_inode_number().read()
+    }
+
     fn view(&self) -> layout::View<&[u8]> {
         layout::View::new(self.0.as_ref())
     }
