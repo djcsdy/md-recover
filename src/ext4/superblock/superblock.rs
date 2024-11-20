a062c1c0a5fa45156617785a4a98070ad002fff4
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-20 19:33:21 +0000

    Add ext4::Superblock::multi_mount_prevention_block function.

--- src/ext4/superblock/superblock.rs
+++ src/ext4/superblock/superblock.rs
@@ -454,6 +454,10 @@ impl<S: AsRef<[u8]>> Superblock<S> {
         self.view().into_multi_mount_prevention_interval().read()
     }
 
+    pub fn multi_mount_prevention_block(&self) -> u64 {
+        self.view().into_multi_mount_prevention_block().read()
+    }
+
     pub fn checksum(&self) -> Checksum {
         let view = self.view();
         match view.checksum_type().read() {
