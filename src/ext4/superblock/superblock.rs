4a8538259fb43b12066effd6fadfa281c4357829
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-20 20:35:20 +0000

    Add ext4::Superblock::first_error_block function.

--- src/ext4/superblock/superblock.rs
+++ src/ext4/superblock/superblock.rs
@@ -523,6 +523,10 @@ impl<S: AsRef<[u8]>> Superblock<S> {
         self.view().into_first_error_inode().read()
     }
 
+    pub fn first_error_block(&self) -> u64 {
+        self.view().into_first_error_block().read()
+    }
+
     fn view(&self) -> layout::View<&[u8]> {
         layout::View::new(self.0.as_ref())
     }
