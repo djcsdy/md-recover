ff71107a06549404017b15c712cda0b02e784ed3
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-20 22:35:52 +0000

    Add ext4::Superblock::last_error_inode function.

--- src/ext4/superblock/superblock.rs
+++ src/ext4/superblock/superblock.rs
@@ -547,6 +547,10 @@ impl<S: AsRef<[u8]>> Superblock<S> {
         }
     }
 
+    pub fn last_error_inode(&self) -> u32 {
+        self.view().into_last_error_inode().read()
+    }
+
     fn view(&self) -> layout::View<&[u8]> {
         layout::View::new(self.0.as_ref())
     }
