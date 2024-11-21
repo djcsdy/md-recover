b9b5905dbc14b4ef38d3910d6677d7f0077141c1
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-11-21 18:58:59 +0000

    Delete unnecessary pub(self).

--- src/ext4/dir_entry/mod.rs
+++ src/ext4/dir_entry/mod.rs
@@ -2,7 +2,7 @@ mod dir_entry_1;
 mod dir_entry_2;
 mod dir_entry_tail;
 
-pub(self) const NAME_LENGTH: usize = 255;
+const NAME_LENGTH: usize = 255;
 
 pub use dir_entry_1::DirEntry1;
 pub use dir_entry_2::DirEntry2;
