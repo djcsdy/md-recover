316ada5b7e7dba47fcde05b5eec78a2d2b30c78b
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-07-01 12:21:32 +0100

    Ext4Fs: Read Superblock when device is opened.

--- src/ext4/fs.rs
+++ src/ext4/fs.rs
@@ -1,11 +1,16 @@
 use crate::block_device::BlockDevice;
+use crate::ext4::superblock::Superblock;
+use std::io::{Result, SeekFrom};
 
 pub struct Ext4Fs<D: BlockDevice> {
     device: D,
+    superblock: Superblock<Vec<u8>>,
 }
 
 impl<D: BlockDevice> Ext4Fs<D> {
-    pub fn open(device: D) -> Self {
-        Self { device }
+    pub fn open(mut device: D) -> Result<Self> {
+        device.seek(SeekFrom::Start(1024))?;
+        let superblock = Superblock::read(&mut device)?;
+        Ok(Self { device, superblock })
     }
 }
