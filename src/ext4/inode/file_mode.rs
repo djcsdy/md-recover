bc95c16d761cd782e15e0b3afb4fb27845c82d7c
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-07-01 14:13:32 +0100

    Implement LayoutAs<u16> for FileMode.

--- src/ext4/inode/file_mode.rs
+++ src/ext4/inode/file_mode.rs
@@ -1,6 +1,8 @@
 use crate::ext4::inode::{FileType, Permissions};
+use binary_layout::LayoutAs;
 use bitflags::Flags;
 use derive_more::with_trait::{From, Into};
+use std::convert::Infallible;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into)]
 pub struct FileMode(u16);
@@ -19,5 +21,18 @@ impl FileMode {
 
     pub fn permissions(&self) -> Permissions {
         Permissions::from_bits_truncate(self.0)
+    }
+}
+
+impl LayoutAs<u16> for FileMode {
+    type ReadError = Infallible;
+    type WriteError = Infallible;
+
+    fn try_read(v: u16) -> Result<Self, Self::ReadError> {
+        Ok(v.into())
+    }
+
+    fn try_write(v: Self) -> Result<u16, Self::WriteError> {
+        Ok(v.into())
     }
 }
