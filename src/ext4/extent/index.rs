d63c3fceb727d166a6ee7c19daf078eaf2349ef5
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-16 00:00:25 +0000

    Add ExtentIndex struct.

---
+++ src/ext4/extent/index.rs
@@ -0,0 +1,28 @@
+use crate::ext::WideUnsigned;
+use binary_layout::binary_layout;
+
+binary_layout!(layout, LittleEndian, {
+    block: u32,
+    leaf_low: u32,
+    leaf_high: u16,
+    unused: u16,
+});
+
+pub struct ExtentIndex<S: AsRef<[u8]>>(layout::View<S>);
+
+impl<S: AsRef<[u8]>> ExtentIndex<S> {
+    pub fn new(storage: S) -> Self {
+        Self(layout::View::new(storage))
+    }
+
+    pub fn block(&self) -> u32 {
+        self.0.block().read()
+    }
+
+    pub fn leaf(&self) -> u64 {
+        u64::from_low_high(
+            self.0.leaf_low().read(),
+            u32::from(self.0.leaf_high().read()),
+        )
+    }
+}
