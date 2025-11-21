b114db562ea8768b884925ff9f21108c8a396c03
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-21 22:47:54 +0000

    Delete obsolete implementation of BlockDevice for Box<dyn BlockDevice>.

--- src/block_device/block_device.rs
+++ src/block_device/block_device.rs
@@ -3,9 +3,3 @@ use std::io::{Read, Result, Seek};
 pub trait BlockDevice: Read + Seek {
     fn size(&self) -> Result<u64>;
 }
-
-impl BlockDevice for Box<dyn BlockDevice> {
-    fn size(&self) -> Result<u64> {
-        (**self).size()
-    }
-}
