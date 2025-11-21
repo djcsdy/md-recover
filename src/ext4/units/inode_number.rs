0af0ea51175ef34d1d9c40cc380600324fd98757
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-21 20:46:20 +0000

    Add InodeCount unit.

--- src/ext4/units/inode_number.rs
+++ src/ext4/units/inode_number.rs
@@ -1,7 +1,21 @@
+use crate::ext4::units::inode_count::InodeCount;
 use derive_more::{Deref, DerefMut, Display, From, Into};
+use std::cmp::Ordering;
 
 #[derive(
     Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display, Deref, DerefMut,
 )]
 #[display("inode #{_0}")]
 pub struct InodeNumber(pub u32);
+
+impl PartialEq<InodeCount> for InodeNumber {
+    fn eq(&self, other: &InodeCount) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl PartialOrd<InodeCount> for InodeNumber {
+    fn partial_cmp(&self, other: &InodeCount) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
--- src/ext4/units/mod.rs
+++ src/ext4/units/mod.rs
@@ -1,6 +1,7 @@
 mod block_count;
 mod file_block_index;
 mod fs_block_index;
+mod inode_count;
 mod inode_number;
 
 #[allow(unused_imports)]
