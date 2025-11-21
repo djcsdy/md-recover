9f777bee1364b4b1915c0b162767eb76c74dc614
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-21 20:51:26 +0000

    Implement PartialEq and PartialOrd<BlockCount<u64>> for FsBlockNumber.

--- src/ext4/units/fs_block_number.rs
+++ src/ext4/units/fs_block_number.rs
@@ -1,7 +1,21 @@
+use crate::ext4::units::BlockCount;
 use derive_more::{Deref, DerefMut, Display, From, Into};
+use std::cmp::Ordering;
 
 #[derive(
     Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display, Deref, DerefMut,
 )]
 #[display("filesystem block #{_0}")]
 pub struct FsBlockNumber(pub u64);
+
+impl PartialEq<BlockCount<u64>> for FsBlockNumber {
+    fn eq(&self, other: &BlockCount<u64>) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl PartialOrd<BlockCount<u64>> for FsBlockNumber {
+    fn partial_cmp(&self, other: &BlockCount<u64>) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
