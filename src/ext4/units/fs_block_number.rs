01eb516c86218d66e721c57b2a42b363d6fc13ed
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-22 03:27:23 +0000

    Implement Add<BlockCount<u16>> for FsBlockNumber.

--- src/ext4/units/fs_block_number.rs
+++ src/ext4/units/fs_block_number.rs
@@ -1,6 +1,7 @@
 use crate::ext4::units::BlockCount;
 use derive_more::{Deref, DerefMut, Display, From, Into};
 use std::cmp::Ordering;
+use std::ops::Add;
 
 #[derive(
     Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display, Deref, DerefMut,
@@ -17,5 +18,13 @@ impl PartialEq<BlockCount<u64>> for FsBlockNumber {
 impl PartialOrd<BlockCount<u64>> for FsBlockNumber {
     fn partial_cmp(&self, other: &BlockCount<u64>) -> Option<Ordering> {
         self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Add<BlockCount<u16>> for FsBlockNumber {
+    type Output = FsBlockNumber;
+
+    fn add(self, rhs: BlockCount<u16>) -> Self::Output {
+        Self(self.0 + u64::from(rhs.0))
     }
 }
