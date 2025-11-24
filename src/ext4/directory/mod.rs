33683909b1bc69c2d83454ecab74e69b158929cb
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-24 18:08:48 +0000

    Delete DirEntry1.
    In practice we can just use DirEntry2 and ignore the file_type field for directories that use the older format.

--- src/ext4/directory/mod.rs
+++ src/ext4/directory/mod.rs
@@ -1,8 +1,7 @@
-mod dir_entry_1;
 mod dir_entry_2;
 mod dir_entry_tail;
 
 const NAME_LENGTH: usize = 255;
 
 #[allow(unused_imports)]
-pub use self::{dir_entry_1::DirEntry1, dir_entry_2::DirEntry2, dir_entry_tail::DirEntryTail};
+pub use self::{dir_entry_2::DirEntry2, dir_entry_tail::DirEntryTail};
