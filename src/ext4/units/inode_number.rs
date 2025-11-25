6cd340c9712f1b7eb09a39d0e3a0f1019117240a
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-25 02:11:28 +0000

    Implement LayoutAs<u32> for InodeNumber.

--- src/ext4/units/inode_number.rs
+++ src/ext4/units/inode_number.rs
@@ -1,6 +1,8 @@
 use crate::ext4::units::inode_count::InodeCount;
+use binary_layout::LayoutAs;
 use derive_more::{Deref, DerefMut, Display, From, Into};
 use std::cmp::Ordering;
+use std::convert::Infallible;
 
 #[derive(
     Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display, Deref, DerefMut,
@@ -17,5 +19,18 @@ impl PartialEq<InodeCount> for InodeNumber {
 impl PartialOrd<InodeCount> for InodeNumber {
     fn partial_cmp(&self, other: &InodeCount) -> Option<Ordering> {
         self.0.partial_cmp(&other.0)
+    }
+}
+
+impl LayoutAs<u32> for InodeNumber {
+    type ReadError = Infallible;
+    type WriteError = Infallible;
+
+    fn try_read(v: u32) -> Result<Self, Self::ReadError> {
+        Ok(Self(v))
+    }
+
+    fn try_write(v: Self) -> Result<u32, Self::WriteError> {
+        Ok(v.0)
     }
 }
