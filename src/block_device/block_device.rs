3fdba1504a7fd5530ec1cd44e7f489b15c8b9d74
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-28 02:49:01 +0000

    Add BlockDevice::block_count function.

--- src/block_device/block_device.rs
+++ src/block_device/block_device.rs
@@ -1,8 +1,16 @@
+use crate::block_device::BlockCount;
 use std::io;
 use std::io::{Read, Seek};
 
 pub trait BlockDevice: Read + Seek + Sized {
     fn block_size(&self) -> io::Result<usize>;
+    fn block_count(&self) -> io::Result<BlockCount> {
+        Ok(BlockCount(
+            self.size()?
+                / u64::try_from(self.block_size()?)
+                    .or(Err(io::Error::from(io::ErrorKind::InvalidData)))?,
+        ))
+    }
     fn size(&self) -> io::Result<u64>;
     fn try_clone(&self) -> io::Result<Self>;
 }
