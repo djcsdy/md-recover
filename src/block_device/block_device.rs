0e59785a54b2ed5aa2e7fa284e81f4ede29a563c
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-28 03:00:58 +0000

    Add BlockDevice::read_block function.

--- src/block_device/block_device.rs
+++ src/block_device/block_device.rs
@@ -1,9 +1,10 @@
-use crate::block_device::BlockCount;
+use crate::block_device::{BlockCount, BlockNumber};
 use std::io;
-use std::io::{Read, Seek};
+use std::io::{Read, Seek, SeekFrom};
 
 pub trait BlockDevice: Read + Seek + Sized {
     fn block_size(&self) -> io::Result<usize>;
+
     fn block_count(&self) -> io::Result<BlockCount> {
         Ok(BlockCount(
             self.size()?
@@ -11,6 +12,23 @@ pub trait BlockDevice: Read + Seek + Sized {
                     .or(Err(io::Error::from(io::ErrorKind::InvalidData)))?,
         ))
     }
+
     fn size(&self) -> io::Result<u64>;
+
+    fn read_block(&mut self, block_number: BlockNumber, buf: &mut [u8]) -> io::Result<usize> {
+        if buf.len() < self.block_size()? {
+            Err(io::ErrorKind::InvalidInput.into())
+        } else {
+            let block_size = self.block_size()?;
+            self.seek(SeekFrom::Start(
+                u64::from(block_number)
+                    * u64::try_from(block_size)
+                        .or(Err(io::Error::from(io::ErrorKind::InvalidData)))?,
+            ))?;
+            self.read_exact(&mut buf[..block_size])?;
+            Ok(block_size)
+        }
+    }
+
     fn try_clone(&self) -> io::Result<Self>;
 }
