fd7eb0aea436e4888f20ac44070b445901d7e94d
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-30 10:23:10 +0000

    Rename md::md_array to md::array.

--- src/md/mod.rs
+++ src/md/mod.rs
@@ -1,13 +1,13 @@
 mod algorithm;
+mod array;
 mod device;
 mod diagnosis;
-mod md_array;
 mod raid5;
 mod raid6;
 pub mod superblock;
 
 #[allow(unused_imports)]
 pub use self::{
+    array::MdArray,
     device::{MdDevice, MdDeviceId, MdDeviceSuperblock},
-    md_array::MdArray,
 };
