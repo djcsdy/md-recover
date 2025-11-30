afb3d3d25a25875c878b6f7444c6caeaee05e1f7
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-11-30 12:05:33 +0000

    Correct BLK_PBSZGET ioctl.

--- src/ioctl/blk.rs
+++ src/ioctl/blk.rs
@@ -3,4 +3,4 @@ use iocuddle::{Group, Ioctl, Read};
 pub const BLK: Group = Group::new(0x12);
 
 pub const BLK_GETSIZE64: Ioctl<Read, &u64> = unsafe { BLK.read(114) };
-pub const BLK_PBSZGET: Ioctl<Read, &u32> = unsafe { BLK.read(123) };
+pub const BLK_PBSZGET: Ioctl<Read, &u32> = unsafe { Ioctl::classic((0x12 << 8) | 123) };
