56e82fc5e245ac387b26c912b7a0159d31c6e6d7
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-12-02 23:23:09 +0000

    md: add MetadataEventCount unit.

--- src/md/units/mod.rs
+++ src/md/units/mod.rs
@@ -1,5 +1,8 @@
 mod device_count;
+mod metadata_event_count;
 mod sector_count;
 
 #[allow(unused_imports)]
-pub use self::{device_count::DeviceCount, sector_count::SectorCount};
+pub use self::{
+    device_count::DeviceCount, metadata_event_count::MetadataEventCount, sector_count::SectorCount,
+};
