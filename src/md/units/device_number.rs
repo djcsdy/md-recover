0b19e4d46e4917a7bbd8c75ddac109a45e05832b
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-12-05 20:26:12 +0000

    md: Add DeviceNumber unit.

---
+++ src/md/units/device_number.rs
@@ -0,0 +1,19 @@
+use crate::md::units::DeviceCount;
+use derive_more::{Display, From, Into};
+use std::cmp::Ordering;
+
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display)]
+#[display("md device #{_0}")]
+pub struct DeviceNumber(pub u32);
+
+impl PartialEq<DeviceCount> for DeviceNumber {
+    fn eq(&self, other: &DeviceCount) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl PartialOrd<DeviceCount> for DeviceNumber {
+    fn partial_cmp(&self, other: &DeviceCount) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
--- src/md/units/mod.rs
+++ src/md/units/mod.rs
@@ -1,5 +1,6 @@
 mod checkpoint_event_count;
 mod device_count;
+mod device_number;
 mod metadata_event_count;
 mod sector_count;
 mod sector_number;
@@ -7,6 +8,6 @@ mod sector_number;
 #[allow(unused_imports)]
 pub use self::{
     checkpoint_event_count::CheckpointEventCount, device_count::DeviceCount,
-    metadata_event_count::MetadataEventCount, sector_count::SectorCount,
-    sector_number::SectorNumber,
+    device_number::DeviceNumber, metadata_event_count::MetadataEventCount,
+    sector_count::SectorCount, sector_number::SectorNumber,
 };
