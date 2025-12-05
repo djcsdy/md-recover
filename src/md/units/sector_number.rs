b80d990a5c6bec815fc91b18e10b8a6b63bf9a24
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-12-05 22:25:04 +0000

    md: Add SectorNumber::in_chunk function.

--- src/md/units/sector_number.rs
+++ src/md/units/sector_number.rs
@@ -1,4 +1,4 @@
-use crate::md::units::SectorCount;
+use crate::md::units::{ChunkNumber, SectorCount};
 use derive_more::{Display, From, Into};
 use std::cmp::Ordering;
 use std::ops::{Add, Sub};
@@ -6,6 +6,21 @@ use std::ops::{Add, Sub};
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display)]
 #[display("sector #{_0}")]
 pub struct SectorNumber(pub u64);
+
+impl SectorNumber {
+    pub fn in_chunk<C>(
+        &self,
+        sectors_per_chunk: SectorCount<C>,
+    ) -> Option<(ChunkNumber, SectorNumber)>
+    where
+        C: PartialOrd + Add + Sub + Copy + Into<u64>,
+    {
+        Some((
+            ChunkNumber(self.0.checked_div(sectors_per_chunk.0.into())?),
+            SectorNumber(self.0.checked_rem(sectors_per_chunk.0.into())?),
+        ))
+    }
+}
 
 impl<C> PartialEq<SectorCount<C>> for SectorNumber
 where
