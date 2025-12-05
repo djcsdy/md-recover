7b10927a6e84ad82d3cffcf9a77f7c589a3f3b90
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-12-05 22:25:29 +0000

    md: Add ChunkNumber::as_data_device_and_stripe_number function.

--- src/md/units/chunk_number.rs
+++ src/md/units/chunk_number.rs
@@ -1,5 +1,23 @@
+use crate::md::units::{DeviceCount, DeviceNumber, StripeNumber};
 use derive_more::{Display, From, Into};
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display)]
 #[display("chunk #{_0}")]
 pub struct ChunkNumber(pub u64);
+
+impl ChunkNumber {
+    pub fn as_data_device_and_stripe_number(
+        &self,
+        data_device_count: DeviceCount,
+    ) -> Option<(DeviceNumber, StripeNumber)> {
+        Some((
+            DeviceNumber(
+                self.0
+                    .checked_rem(u64::from(data_device_count))?
+                    .try_into()
+                    .ok()?,
+            ),
+            StripeNumber(self.0.checked_div(u64::from(data_device_count))?),
+        ))
+    }
+}
