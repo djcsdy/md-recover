2b619fe0f23d050d3a99418322c1bb7b56b12d82
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-12-06 15:28:38 +0000

    md: Add prototype implementation of BlockDevice for MdArray.

--- src/md/array.rs
+++ src/md/array.rs
@@ -1,10 +1,12 @@
-use crate::block_device::BlockDevice;
+use crate::block_device::{BlockCount, BlockDevice, BlockDeviceReader, BlockNumber, BlockSize};
 use crate::md::config::MdConfig;
 use crate::md::definition::MdArrayDefinition;
 use crate::md::diagnosis::Diagnosis;
+use crate::md::units::SectorNumber;
 use crate::md::MdDevice;
 use itertools::Itertools;
-use std::io::{Read, Seek};
+use std::io;
+use std::io::{Read, Seek, SeekFrom};
 use std::rc::Rc;
 
 pub struct MdArray<D>
@@ -29,5 +31,72 @@ where
 
     pub fn diagnose(&self) -> Diagnosis {
         self.definition.diagnose()
+    }
+}
+
+impl<D> BlockDevice for MdArray<D>
+where
+    D: BlockDevice + Read + Seek,
+{
+    fn block_size(&self) -> io::Result<BlockSize> {
+        Ok(BlockSize(512))
+    }
+
+    fn block_count(&self) -> io::Result<BlockCount> {
+        Ok(self
+            .definition
+            .config
+            .as_ref()
+            .ok_or(io::ErrorKind::InvalidData)?
+            .data_sector_count()
+            .ok_or(io::ErrorKind::InvalidData)?
+            .as_block_count())
+    }
+
+    fn read_block(&mut self, block_number: BlockNumber, buf: &mut [u8]) -> io::Result<usize> {
+        if buf.len() < 512 {
+            Err(io::ErrorKind::InvalidInput)?;
+        }
+
+        let config = self
+            .definition
+            .config
+            .as_ref()
+            .ok_or(io::ErrorKind::InvalidData)?;
+
+        let block = config.algorithm.read_sector(
+            SectorNumber::from_block_number(block_number),
+            config.chunk_size,
+            config.device_count,
+            |device_number, sector_number, buf| {
+                if buf.len() < 512 {
+                    Err(io::ErrorKind::InvalidInput)?;
+                }
+
+                let device = self
+                    .definition
+                    .devices
+                    .get(usize::from(device_number))
+                    .ok_or(io::ErrorKind::InvalidInput)?
+                    .as_ref()
+                    .try_clone()?;
+                let mut reader = BlockDeviceReader::new(device);
+                reader.seek(SeekFrom::Start(
+                    u64::from(sector_number)
+                        .checked_mul(512)
+                        .ok_or(io::ErrorKind::InvalidInput)?,
+                ))?;
+                reader.read_exact(&mut buf[..512])?;
+                Ok(512)
+            },
+        )?;
+        buf.copy_from_slice(&block);
+        Ok(512)
+    }
+
+    fn try_clone(&self) -> io::Result<Self> {
+        Ok(Self {
+            definition: self.definition.clone(),
+        })
     }
 }
