c24de11e7ce02139bb4b394061f90368b379ade4
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-08-31 21:01:05 +0100

    clippy: Disallow numeric casts that lose data.

--- src/ext4/inode/time.rs
+++ src/ext4/inode/time.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Duration, Utc};
 
 pub fn decode_extra_time(base: u32, extra: u32) -> DateTime<Utc> {
-    let seconds = i64::from(base as i32) + ((i64::from(extra) & 3) << 32);
+    let seconds = i64::from(base.cast_signed()) + ((i64::from(extra) & 3) << 32);
     let nanos = i64::from(extra >> 2);
     DateTime::UNIX_EPOCH + Duration::seconds(seconds) + Duration::nanoseconds(nanos)
 }
