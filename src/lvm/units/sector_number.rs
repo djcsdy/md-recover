9f219d456928286bdd317ccad119fa9dbf2132d5
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-08-31 05:21:16 +0100

    lvm: Add SectorNumber::byte_pos() function.

--- src/lvm/units/sector_number.rs
+++ src/lvm/units/sector_number.rs
@@ -1,5 +1,13 @@
+use crate::lvm::SECTOR_SIZE_BYTES;
 use derive_more::{Display, From, Into};
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug, From, Into, Display)]
 #[display("sector #{_0}")]
 pub struct SectorNumber(pub u64);
+
+impl SectorNumber {
+    pub(in crate::lvm) fn byte_pos(&self) -> Option<u64> {
+        self.0
+            .checked_mul(u64::try_from(SECTOR_SIZE_BYTES).unwrap())
+    }
+}
