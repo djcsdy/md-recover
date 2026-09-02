dc1c2b8ecff3dd9a239377c3bce34a9c24258ec0
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-09-02 22:43:52 +0100

    lvm: PhysicalVolumeHeader: Add missing uuid.

--- src/lvm/physical_volume/header.rs
+++ src/lvm/physical_volume/header.rs
@@ -5,6 +5,7 @@ use std::io::Read;
 
 #[derive(Clone)]
 pub struct PhysicalVolumeHeader {
+    uuid: [u8; 32],
     device_size_bytes: u64,
     data_areas: Vec<DiskLocation<Vec<u8>>>,
     metadata_areas: Vec<DiskLocation<Vec<u8>>>,
@@ -12,11 +13,15 @@ pub struct PhysicalVolumeHeader {
 
 impl PhysicalVolumeHeader {
     pub fn read<R: Read>(mut reader: R) -> io::Result<Self> {
+        let mut uuid = [0u8; 32];
+        reader.read_exact(&mut uuid)?;
+
         let device_size_bytes = reader.read_u64::<LittleEndian>()?;
         let data_areas = DiskLocation::read_all(&mut reader)?;
         let metadata_areas = DiskLocation::read_all(&mut reader)?;
 
         Ok(Self {
+            uuid,
             device_size_bytes,
             data_areas,
             metadata_areas,
