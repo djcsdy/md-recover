23609578061b5590de98420152919bb4d248c1d2
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-09-19 09:25:35 +0100

    lvm: Delete unused MetadataValue::parse().

--- src/lvm/metadata/value.rs
+++ src/lvm/metadata/value.rs
@@ -1,8 +1,4 @@
-use crate::lvm::metadata::parse;
 use std::collections::HashMap;
-use winnow::error::{ContextError, ParseError};
-use winnow::stream::{AsChar, Compare, ParseSlice, Stream, StreamIsPartial};
-use winnow::Parser;
 
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub enum MetadataValue {
@@ -10,17 +6,4 @@ pub enum MetadataValue {
     Integer(u64),
     Array(Vec<MetadataValue>),
     Object(HashMap<String, MetadataValue>),
-}
-
-impl MetadataValue {
-    pub fn parse<Input>(
-        input: Input,
-    ) -> Result<HashMap<String, MetadataValue>, ParseError<Input, ContextError>>
-    where
-        Input: StreamIsPartial + Stream + Compare<char>,
-        <Input as Stream>::Slice: ParseSlice<u64> + ParseSlice<String>,
-        <Input as Stream>::Token: AsChar + Clone,
-    {
-        parse::metadata::<Input, ContextError>.parse(input)
-    }
 }
